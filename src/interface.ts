9cb4c0562e690cef9a9070ad8f76a8fca264aa5c
t532 <2037246484@qq.com>
2019-12-29 03:25:59 +0800

    feat(interface): add common interfaces Raw, Sieveable
    Raw means a type can be converted into a certain type of raw event object by raw(); Sieveable means
    a type can be converted to an array of CQCodes.

---
+++ src/interface.ts
@@ -0,0 +1,16 @@
+import { Extensive } from './util'
+import { Code, toArray } from './code'
+
+export interface Raw<T> { _raw: T }
+export type MaybeRaw<T> = Raw<T> | T
+export const raw = <T>(x: MaybeRaw<T>) => ('raw' in x ? (x as Raw<T>)._raw : x) as T
+
+export interface Sieveable { _sieveable: Code[] | string }
+export type MaybeSieveable = Extensive<{ _sieveable: Code[] | string }> | Code[] | string
+export const sieveable = (x: MaybeSieveable) => {
+    if (typeof x === 'string')
+        return toArray(x)
+    if (x instanceof Array)
+        return x
+    return sieveable(x._sieveable)
+}
