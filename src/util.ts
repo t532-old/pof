bb3e356b926e9bddc41cc6b6d64d7979eae43ac9
t532 <2037246484@qq.com>
2019-12-29 03:22:52 +0800

    improvement(util): add kvFilter, kvMap, merge; fix defined, flipOut

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,9 @@
-export const defined = (x: any) => x !== undefined
+export const defined = (x: any) => x !== null && x !== undefined 
 
 export const flipOut = (x: object, center: string) => {
+    if (!defined(x[center])) {
+        x[center] = {}
+    }
     Object.entries(x).forEach(([k, v]) =>
         k === center || (x[center][`$${k}`] = v))
     return x[center]
@@ -8,4 +11,18 @@ export const flipOut = (x: object, center: string) => {
 
 export const noop = (...args: any[]) => {}
 
+type KvPair = [string, any]
+type Kv = Record<string, any>
+
+export const merge = (...xs: any[]) =>
+    Object.fromEntries(xs.map(Object.entries).flat())
+
+export const kvMap = (x: Kv) => (f: (kv: KvPair, idx: number, entries: KvPair[]) => KvPair) =>
+    Object.fromEntries(Object.entries(x).map(f))
+
+export const kvFilter = (x: Kv) => (f: (kv: KvPair, idx: number, entries: KvPair[]) => boolean) =>
+    Object.fromEntries(Object.entries(x).filter(f))
+
 export type MaybePromise<T> = T | Promise<T>
+
+export type Extensive<T> = T & Record<PropertyKey, any>
