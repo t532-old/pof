2b35ce3910658f448952fdce0bc50d13ec34bbc2
t532 <2037246484@qq.com>
2019-12-29 03:23:40 +0800

    refactor(code): merge code.api.ts into code.ts

--- src/code.ts
+++ src/code.ts
@@ -1,8 +1,27 @@
-import { CQObject } from './code.definition'
-
 export interface Code {
     type: string
     data: any
+}
+
+type ToCode<N, T> = (x?: T) => { type: N, data: T }
+
+type CQObject = {
+    text: ToCode<'text', { text: string }>
+    at: ToCode<'at', { qq: number }>
+    anonymous: ToCode<'anonymous', { ignore: boolean }>
+    face: ToCode<'face', { id: number }>
+    emoji: ToCode<'emoji', { id: number }>
+    bface: ToCode<'bface', { id: number }>
+    sface: ToCode<'sface', { id: number }>
+    record: ToCode<'record', { file: string, magic?: boolean }>
+    image: ToCode<'image', { file: string }>
+    music: ToCode<'music',
+        { type: 'qq' | '163', id: number } |
+        { type: 'custom', url: string, audio: string, title: string, content?: string, image?: string }>
+    share: ToCode<'share', { url: string, title: string, content?: string, image?: string }>
+    rps: ToCode<'rps', null>
+    dice: ToCode<'dice', null>
+    shake: ToCode<'shake', null>
 }
 
 export const CQ = new Proxy({}, {
