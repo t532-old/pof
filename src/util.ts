70af908250ff0b1c58a8de21c73024db54564ba9
t532 <2037246484@qq.com>
2020-01-06 00:40:32 +0800

    improvement(util): improve merge(), some slight code changes as well

--- src/util.ts
+++ src/util.ts
@@ -9,19 +9,26 @@ export const flipOut = (x: object, center: string) => {
     return x[center]
 }
 
-export const noop = (...args: any[]) => {}
+export const noop = (..._: any[]) => {}
 
 type KvPair = [string, any]
 type Kv = Record<string, any>
 
-export const merge = (...xs: any[]) =>
-    Object.fromEntries(xs.map(Object.entries).flat())
+export const merge = <T extends object, U>(x: T, y: U) =>
+    Object.fromEntries([
+        ...Object.entries(x), 
+        ...Object.entries(y)
+    ]) as T & U
 
-export const kvMap = (x: Kv) => (f: (kv: KvPair, idx: number, entries: KvPair[]) => KvPair) =>
-    Object.fromEntries(Object.entries(x).map(f))
+type Mapping = (kv: KvPair, idx: number, entries: KvPair[]) => KvPair
+export const kvMap = (x: Kv) => (f: Mapping) =>
+    Object.fromEntries(
+        Object.entries(x).map(f))
 
-export const kvFilter = (x: Kv) => (f: (kv: KvPair, idx: number, entries: KvPair[]) => boolean) =>
-    Object.fromEntries(Object.entries(x).filter(f))
+type Predicate = (kv: KvPair, idx: number, entries: KvPair[]) => boolean
+export const kvFilter = (x: Kv) => (f: Predicate) =>
+    Object.fromEntries(
+        Object.entries(x).filter(f))
 
 export type MaybePromise<T> = T | Promise<T>
 
